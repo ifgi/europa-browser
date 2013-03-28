05f6b9c1b364ded8151e8f4aead0f92b519b136e
Jim Jones <jimjonesbr@gmail.com>
2013-03-28 22:43:06 +0100

    Updated constants.

--- europa/src/main/java/de/ifgi/europa/constants/Constants.java
+++ europa/src/main/java/de/ifgi/europa/constants/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	/**
 	 * Address to the SPARQL Endpoint 
 	 */
-	public static String SPARQL_Endpoint = "http://localhost:8080/parliament/sparql";
+	public static String SPARQL_Endpoint = "http://giv-siidemo.uni-muenster.de:8081/parliament/sparql";
 	
 }
