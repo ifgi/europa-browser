f0c51dff602fa245fee3d15441f060db0a048e1b
Jim Jones <jimjonesbr@gmail.com>
2013-03-28 22:38:03 +0100

    Added Jena testing samples.

---
+++ europa/src/main/java/de/ifgi/europa/constants/Constants.java
@@ -0,0 +1,10 @@
+package de.ifgi.europa.constants;
+
+public class Constants {
+
+	/**
+	 * Address to the SPARQL Endpoint 
+	 */
+	public static String SPARQL_Endpoint = "http://localhost:8080/parliament/sparql";
+	
+}
---
+++ europa/src/main/java/de/ifgi/europa/tests/QueryLocalParliament.java
@@ -0,0 +1,43 @@
+package de.ifgi.europa.tests;
+
+import com.hp.hpl.jena.query.ARQ;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+
+import de.ifgi.europa.constants.Constants;
+
+public class QueryLocalParliament {
+
+    public static void main(String[] args) {
+
+        QueryLocalParliament queryDBpedia = new QueryLocalParliament();
+        queryDBpedia.queryExternalSources();
+    }
+
+    public void queryExternalSources() {
+        //Defining SPARQL Query. This query lists, in all languages available, the
+        //abstract entries on Wikipedia/DBpedia for the planet Mars.
+        String sparqlQueryString2 = " SELECT  ?s  " +
+                                    " WHERE {?s ?p <http://purl.oclc.org/NET/ssnx/ssn#Sensor>}";
+
+        Query query = QueryFactory.create(sparqlQueryString2);
+        ARQ.getContext().setTrue(ARQ.useSAX);
+       //Executing SPARQL Query and pointing to the DBpedia SPARQL Endpoint 
+        QueryExecution qexec = QueryExecutionFactory.sparqlService(Constants.SPARQL_Endpoint, query);
+       //Retrieving the SPARQL Query results
+        ResultSet results = qexec.execSelect();
+       //Iterating over the SPARQL Query results
+        while (results.hasNext()) {
+            QuerySolution soln = results.nextSolution();
+            //Printing DBpedia entries' abstract.
+            System.out.println(soln.get("?s"));                                                
+        }
+        qexec.close();
+
+    }
+
+}
