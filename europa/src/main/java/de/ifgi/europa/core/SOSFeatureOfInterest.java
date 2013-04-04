adb97f1521ff92f8d10f3d98ebb60b82273a233d
Jim Jones <jimjonesbr@gmail.com>
2013-04-04 17:56:05 +0200

    .

--- europa/src/main/java/de/ifgi/europa/core/SOSFeatureOfInterest.java
+++ europa/src/main/java/de/ifgi/europa/core/SOSFeatureOfInterest.java
@@ -2,6 +2,7 @@ package de.ifgi.europa.core;
 
 public class SOSFeatureOfInterest {
 
+<<<<<<< HEAD
 	private long id;
 	private String identifier;
 	private String name;
@@ -57,5 +58,57 @@ public class SOSFeatureOfInterest {
 	}
 	
 	
+=======
+	long id;
+	String identifier;
+	String name;
+	String label;	
+	SOSPoint defaultGeometry;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getIdentifier() {
+		return identifier;
+	}
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public SOSPoint getDefaultGeometry() {
+		return defaultGeometry;
+	}
+	public void setDefaultGeometry(SOSPoint defaultGeometry) {
+		this.defaultGeometry = defaultGeometry;
+	}
+	
+	public SOSFeatureOfInterest(long id, String identifier, String name,
+			String label, SOSPoint defaultGeometry) {
+		super();
+		this.id = id;
+		this.identifier = identifier;
+		this.name = name;
+		this.label = label;
+		this.defaultGeometry = defaultGeometry;
+	}
+	
+	public SOSFeatureOfInterest() {
+		super();
+	}	
+>>>>>>> refs/remotes/origin/master
 	
 }
