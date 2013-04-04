323e6304a61c6bdc42f518a80297259896baff30
Jim Jones <jimjonesbr@gmail.com>
2013-04-04 16:15:38 +0200

    Added: SOSFeatureOfInterest, SOSPoint.

--- europa/src/main/java/de/ifig/europa/core/SOSObservation.java
+++ europa/src/main/java/de/ifig/europa/core/SOSObservation.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 public class SOSObservation {
 
 	ArrayList<SOSValue> values;
-	FeatureOfInterest FOI;
+	SOSFeatureOfInterest FOI;
 	
 }
--- europa/src/main/java/de/ifig/europa/core/SOSValue.java
+++ europa/src/main/java/de/ifig/europa/core/SOSValue.java
@@ -4,46 +4,9 @@ import java.util.Date;
 
 public class SOSValue {
 
-	String value;
-	String unitOfMeasurements;
-	Date samplingTime;
-	String property;
-	
-	public SOSValue(String value, String unitOfMeasurements, Date samplingTime,
-			String property) {
-		super();
-		this.value = value;
-		this.unitOfMeasurements = unitOfMeasurements;
-		this.samplingTime = samplingTime;
-		this.property = property;
-	}
-	
-
-	public String getProperty() {
-		return property;
-	}
-	public void setProperty(String property) {
-		this.property = property;
-	}
-	public String getValue() {
-		return value;
-	}
-	public void setValue(String value) {
-		this.value = value;
-	}
-	public String getUnitOfMeasurements() {
-		return unitOfMeasurements;
-	}
-	public void setUnitOfMeasurements(String unitOfMeasurements) {
-		this.unitOfMeasurements = unitOfMeasurements;
-	}
-	public Date getSamplingTime() {
-		return samplingTime;
-	}
-	public void setSamplingTime(Date samplingTime) {
-		this.samplingTime = samplingTime;
-	}
-	
+	long id;
+	double hasValue;
+		
 
 
 	
