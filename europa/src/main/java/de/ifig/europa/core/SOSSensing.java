85fef9836edc25f99f4b9de47999f6a34f57d488
Alber Sanchez <albhasan@gmail.com>
2013-04-04 16:51:34 +0200

    SOSSensing class creation

---
+++ europa/src/main/java/de/ifig/europa/core/SOSSensing.java
@@ -0,0 +1,22 @@
+package de.ifig.europa.core;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+public class SOSSensing {
+
+	private ArrayList<URI> description;
+	private String label;
+
+	public SOSSensing(ArrayList<URI> description, String label) {
+		super();
+		this.description = description;
+		this.label = label;
+	}
+
+	public SOSSensing() {
+		super();
+	}
+
+
+}
