6c005d1cfa9e992c00b98de4312bb403719711a0
Alber Sanchez <albhasan@gmail.com>
2013-04-09 15:51:39 +0200

    New development on FACT

--- europa/src/main/java/de/ifgi/europa/factory/LODResourceFactory.java
+++ europa/src/main/java/de/ifgi/europa/factory/LODResourceFactory.java
@@ -2,11 +2,12 @@ package de.ifgi.europa.factory;
 
 import java.net.URI;
 
+import de.ifgi.europa.constants.Constants;
 import de.ifgi.europa.core.LODResource;
 
 public class LODResourceFactory {
 
-	protected LODResource create(URI uri){
+	protected LODResource create(URI uri,Constants.FactTypes type){
 		return null;
 	}
 
