fff33346bcfa2e890b6cac0ff9864875cf586ced
Matthias Pfeil <matthias.pfeil@wwu.de>
2013-05-10 12:21:45 +0200

    Added querying function

--- europa/src/main/java/de/ifgi/europa/gui/MapPanel.java
+++ europa/src/main/java/de/ifgi/europa/gui/MapPanel.java
@@ -1,24 +1,115 @@
 package de.ifgi.europa.gui;
 
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import gov.nasa.worldwind.BasicModel;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.event.SelectEvent;
+import gov.nasa.worldwind.event.SelectListener;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.globes.Earth;
+import gov.nasa.worldwind.globes.Globe;
+import gov.nasa.worldwind.layers.CompassLayer;
+import gov.nasa.worldwind.layers.Layer;
+import gov.nasa.worldwind.layers.LayerList;
+import gov.nasa.worldwind.layers.MarkerLayer;
+import gov.nasa.worldwind.layers.ScalebarLayer;
+import gov.nasa.worldwind.layers.StarsLayer;
+import gov.nasa.worldwind.layers.ViewControlsLayer;
+import gov.nasa.worldwind.layers.WorldMapLayer;
+import gov.nasa.worldwind.layers.Earth.BMNGWMSLayer;
+import gov.nasa.worldwind.layers.Earth.LandsatI3WMSLayer;
+import gov.nasa.worldwind.render.Material;
+import gov.nasa.worldwind.render.markers.BasicMarker;
+import gov.nasa.worldwind.render.markers.BasicMarkerAttributes;
+import gov.nasa.worldwind.render.markers.BasicMarkerShape;
+import gov.nasa.worldwind.render.markers.Marker;
 
 import javax.swing.JPanel;
 
 public class MapPanel extends JPanel {
 
 	private MainFrame mainFrame;
+	final MarkerLayer layer = new MarkerLayer();
+	final LayerList ll = new LayerList();
+	final WorldWindowGLCanvas wwd = new WorldWindowGLCanvas();
 	
 	public MapPanel(MainFrame mF) {
 		super(new GridLayout(1, 1));
 		this.setMainFrame(mF);
 		
-		WorldWindowGLCanvas wwd = new WorldWindowGLCanvas();
+        layer.setOverrideMarkerElevation(true);
+        layer.setKeepSeparated(false);
+        layer.setElevation(1000d);
+		
+		Layer[] layers = new Layer[]
+        {
+			new ViewControlsLayer(),
+            new StarsLayer(),
+            new CompassLayer(),
+            new BMNGWMSLayer(),
+            new LandsatI3WMSLayer(),
+            layer,
+            new ScalebarLayer(),
+            new WorldMapLayer(),
+        };
+		
+		Globe earth = new Earth();
+		BasicModel modelForWindowA = new BasicModel();
+		modelForWindowA.setGlobe(earth);
+		modelForWindowA.setLayers(new LayerList(layers));
         wwd.setPreferredSize(new java.awt.Dimension(800, 600));
+        wwd.setModel(modelForWindowA);
         this.add(wwd, java.awt.BorderLayout.CENTER);
-        wwd.setModel(new BasicModel());
+        
+        wwd.addMouseListener(new MouseListener() {
+			
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ArrayList<Marker> markers = new ArrayList<Marker>();
+		        Marker marker = new BasicMarker(Position.fromDegrees(52, 7, 0), new BasicMarkerAttributes(Material.YELLOW, BasicMarkerShape.ORIENTED_CYLINDER_LINE, 0.9));
+		        marker.setPosition(Position.fromDegrees(52, 7, 0));
+		        marker.setHeading(Angle.fromDegrees(52 * 5));
+		        markers.add(marker);
+		        
+		        layer.setMarkers(markers);
+			}
+		});
+        
+        wwd.addSelectListener(new SelectListener() {
+			
+			public void selected(SelectEvent arg0) {
+				System.out.println("selected");
+			}
+		});
+	}
+	
+	public void updateGlobe() {
+		System.out.println("updateGlobe");
 	}
 
 	public MainFrame getMainFrame() {
